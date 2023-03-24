33ab04a0108a97f0e4f32739ce67833370c2a9fd
Vitalii Magomedov <vitalikmag28@gmail.com>
2023-03-24 16:37:06 +0300

    refactoring

--- store/settings.py
+++ store/settings.py
@@ -139,13 +139,13 @@ CACHES = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": env('DATABASE_NAME'),
-        "USER": env('DATABASE_USER'),
-        "PASSWORD": env('DATABASE_PASSWORD'),
-        "HOST": env('DATABASE_HOST'),
-        "PORT": env('DATABASE_PORT'),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
     }
 }
 
--- users/forms.py
+++ users/forms.py
@@ -19,17 +19,17 @@ class UserLoginForm(AuthenticationForm):
 
 class UserRegistrationForm(UserCreationForm):
     first_name = forms.CharField(widget=forms.TextInput(attrs={
-        'class': "form-control py-4", 'placeholder': "Введите имя"}))
+        'class': 'form-control py-4', 'placeholder': 'Введите имя'}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={
-        'class': "form-control py-4", 'placeholder': "Введите фамилию"}))
+        'class': 'form-control py-4', 'placeholder': 'Введите фамилию'}))
     username = forms.CharField(widget=forms.TextInput(attrs={
-        'class': "form-control py-4", 'placeholder': "Введите имя пользователя"}))
+        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'}))
     email = forms.CharField(widget=forms.EmailInput(attrs={
-        'class': "form-control py-4", 'placeholder': "Введите email"}))
+        'class': 'form-control py-4', 'placeholder': 'Введите email'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': "form-control py-4", 'placeholder': "Введите пароль"}))
+        'class': 'form-control py-4', 'placeholder': 'Введите пароль'}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': "form-control py-4", 'placeholder': "Подтвердите пароль"}))
+        'class': 'form-control py-4', 'placeholder': 'Подтвердите пароль'}))
 
     class Meta:
         model = User
@@ -43,14 +43,14 @@ class UserRegistrationForm(UserCreationForm):
 
 class UserProfileForm(UserChangeForm):
     first_name = forms.CharField(widget=forms.TextInput(attrs={
-        'class': "form-control py-4"}))
+        'class': 'form-control py-4'}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={
-        'class': "form-control py-4"}))
-    image = forms.ImageField(widget=forms.FileInput(attrs={'class': "custom-file-input"}), required=False)
+        'class': 'form-control py-4'}))
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}), required=False)
     username = forms.CharField(widget=forms.TextInput(attrs={
-        'class': "form-control py-4", 'readonly': True}))
+        'class': 'form-control py-4', 'readonly': True}))
     email = forms.CharField(widget=forms.TextInput(attrs={
-        'class': "form-control py-4", 'readonly': True}))
+        'class': 'form-control py-4', 'readonly': True}))
 
     class Meta:
         model = User
